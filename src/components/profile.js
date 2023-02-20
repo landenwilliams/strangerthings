93a7d110ecbd400a80c8670805a2487c34b9b5fe
landen <landenjwilliams@gmail.com>
2023-02-20 12:36:17 -0600

    pretty much fully functional, weird double message bug

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,22 +1,24 @@
 import React, {useState, useEffect} from "react";
+import { render } from "react-dom";
 import { Navigate} from 'react-router-dom';
 
 const Profile = (props) => {
 
     const [ profile, setProfile ] = useState([]);
     const [ profileLoaded, setProfileLoaded] = useState (false);
+    const [ editPostDisplay, setEditPostDisplay ] = useState(false);
+    const [ postTitle, setPostTitle] = useState('');
+    const [ postDescript, setPostDescript ] = useState('');
+    const [ postPrice, setPostPrice ] = useState('');
+    const [ postLocation, setPostLocation ] = useState('');
+    const [ currentPost, setCurrentPost ] = useState({});
     const token = window.localStorage.getItem('token');
-
    
     // console.log(token);
 
     useEffect (() => {
 
-        
-        
-
         const fetchProfile = async() => {
-
 
             fetch('https://strangers-things.herokuapp.com/api/2211-ftb-et-web-am/users/me', {
             headers: {
@@ -47,9 +49,40 @@ const deletePost = (post, index) => {
     }).then(response => response.json())
     .then(result => {
     console.log(result);
+    window.location.reload();
+    })
+    .catch(console.error);   
+
+}
+
+
+    const editPost = (post, index) => {
+    fetch(`https://strangers-things.herokuapp.com/api/2211-ftb-et-web-am/posts/${post._id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+        post: {
+            title: `${postTitle}`,
+            description: `${postDescript}`,
+            price: `${postPrice}`,
+            location: `${postLocation}`,
+            willDeliver: true
+        }
+        })
+    }).then(response => response.json())
+    .then(result => {
+    console.log(result);
+    window.location.reload();
     })
     .catch(console.error);
 
+    }
+const editOnClick = (post) => {
+    setCurrentPost (post);
+    setEditPostDisplay(true);
 }
 
 
@@ -61,23 +94,35 @@ const deletePost = (post, index) => {
             { props.isLoggedIn ? <>
                 
                 { profileLoaded ? <> <h1>Username: {profile.username}</h1><br/><h2>Posts: </h2>
-                    { profile.posts.map((post, index) => {
-                        
-                      
+                    { profile.posts.map((post, index) => { 
+                        // () => {setPostTitle(post.title)}
                         return (
                             
-                            <React.Fragment key={index} > 
+                            <React.Fragment key={index} >         
+
                                 { post.active ? <>
-                                <div id="post" >
-                                    <h1>Title: {post.title}</h1>
-                                    <h2>Price: {post.price}</h2>
-                                    <h3>Description:{post.description}</h3>
-                                    <h4>Location:{post.location}</h4>
-                                    <h5>Will Deliver: { post.willDeliver ? <>Yes</> : <>No</> } </h5>
-                                    <h5>Posted: {post.createdAt}</h5>
-                                    <button>Edit</button><button onClick={()=>deletePost(post, index)}>Delete</button>
-                                </div><br/></>
+                                    { editPostDisplay ? 
+                                        <div className="popupbox">
+                                            <div><h1>Edit Post:</h1>
+                                                <input placeholder={currentPost.title} defaultValue={currentPost.title} onChange={() => {setPostTitle(event.target.value)}}/><br/>
+                                                <input placeholder={currentPost.description} defaultValue={currentPost.description} onChange={() =>{setPostDescript(event.target.value)}}/><br/>
+                                                <input placeholder={currentPost.price} type="number" defaultValue={currentPost.price} onChange={() => {setPostPrice(event.target.value)}}/><br/>
+                                                <input placeholder={currentPost.location} defaultValue={currentPost.location} onChange={() => {setPostLocation(event.target.value)}}/><br/>
+                                                <button className="postbuttons" onClick={() => editPost(currentPost)}>Submit Edit</button>
+                                                <button className="postbuttons" onClick={() => setEditPostDisplay(false)}>Cancel</button>
+                                            </div>
+                                        </div>  : null } 
+                                        <div id="post" >
+                                            <h1>Title: {post.title}</h1>
+                                            <h2>Price: {post.price}</h2>
+                                            <h3>Description:{post.description}</h3>
+                                            <h4>Location:{post.location}</h4>
+                                            <h5>Will Deliver: { post.willDeliver ? <>Yes</> : <>No</> } </h5>
+                                            <h5>Posted: {post.createdAt}</h5>
+                                            <button onClick={()=>editOnClick(post)}>Edit</button><button onClick={()=>deletePost(post, index)}>Delete</button>
+                                        </div><br/></>
                                  : null } 
+                                 
                             </React.Fragment>
                             
                         )
@@ -92,7 +137,7 @@ const deletePost = (post, index) => {
                                     <h1>From: {post.fromUser.username}</h1>
                                     <h2>Post: {post.post.title}</h2>
                                     <h3>Message: {post.content}</h3>
-                                    <button>Reply</button><button>Delete</button>
+                                    <button>Reply</button>
                                 </div><br/>
                             </React.Fragment>
                         )
@@ -103,7 +148,5 @@ const deletePost = (post, index) => {
         </> : <Navigate to="/" /> }        
         </div>)
 }
-
-
 
 export default Profile;
--- src/components/renderpage.js
+++ src/components/renderpage.js
@@ -9,9 +9,10 @@ const RenderPage = (props) => {
     const [ postDescript, setPostDescript ] = useState('');
     const [ postPrice, setPostPrice ] = useState('');
     const [ postLocation, setPostLocation ] = useState('');
+    const [ messageContent, setMessageContent ] = useState('');
+    const [ currentPost, setCurrentPost ] = useState({});
 
     const token = window.localStorage.getItem('token');
-
 
     const addPost = () => {
         fetch('https://strangers-things.herokuapp.com/api/2211-ftb-et-web-am/posts', {
@@ -28,39 +29,63 @@ const RenderPage = (props) => {
         location: `${postLocation}`,
         willDeliver: true
         }
-  })
-}).then(response => response.json())
-  .then(result => {
-    console.log(result);
-  })
-  .catch(console.error);
+        })
+        }).then(response => response.json())
+        .then(result => {
+        console.log(result);
+        })
+        .catch(console.error);
         
     }
 
-  
-    
+    const sendMessage = async (e) => {
+        setAddMessageDisplay(false);
+        e.preventDefault();
+        fetch(`https://strangers-things.herokuapp.com/api/2211-ftb-et-web-am/posts/${currentPost._id}/messages`, {
+        method: "POST",
+         headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+        message: {
+        content: `${messageContent}`
+        }
+        })
+        }).then(response => response.json())
+        .then(result => {
+        console.log(result);
+        
+        })
+        .catch(console.error);
+        }
+
+    const messageOnClick = (post) => {
+        setAddMessageDisplay(true);
+        setCurrentPost(post);
+    }
     return (
         <>
         { addPostDisplay ? 
-            <div id="addpost">
+            <div className="popupbox">
                 <div>
                     <input placeholder="Title" onChange={() => {setPostTitle(event.target.value)}}/><br/>
                     <input placeholder="Description" onChange={() =>{setPostDescript(event.target.value)}}/><br/>
                     <input placeholder="Price" type="number" onChange={() => {setPostPrice(event.target.value)}}/><br/>
                     <input placeholder="Location" onChange={() => {setPostLocation(event.target.value)}}/><br/>
                     
-                    <Link to='/'><button className="postbuttons" onClick={addPost}>Create Post</button></Link>
+                    <button className="postbuttons" onClick={addPost}>Create Post</button>
                     <button className="postbuttons" onClick={() => setAddPostDisplay(false)}>Cancel</button>
                     
                 </div>
             </div> : null} 
 
         { addMessageDisplay ? 
-            <div id="addpost">
+            <div className="popupbox">
                 <div>
-                    <input placeholder="Message" /><br/>
+                    <input placeholder="Message" onChange={() => {setMessageContent(event.target.value)}}/><br/>
                     
-                    <Link to='/'><button className="postbuttons" >Send</button></Link>
+                    <Link to='/'><button className="postbuttons" onClick={sendMessage}>Send</button></Link>
                     <button className="postbuttons" onClick={() => setAddMessageDisplay(false)}>Cancel</button>
                     
                 </div>
@@ -77,8 +102,9 @@ const RenderPage = (props) => {
                 }).map((post, index) => {
                     return (
                             <React.Fragment key={index}>
+                                
                                 <div id="post" >
-                                    <div id='postUsername'><h5>Username: {post.author.username}</h5> { props.isLoggedIn ? <button onClick={() => { setAddMessageDisplay(true)}}>Message</button> : null } </div>
+                                    <div id='postUsername'><h5>Username: {post.author.username}</h5> { props.isLoggedIn ? <button onClick={() => { messageOnClick(post)}}>Message</button> : null } </div>
                                     <h1>Title: {post.title}</h1>
                                     <h2>Price: {post.price}</h2>
                                     <h3>Description:{post.description}</h3>
